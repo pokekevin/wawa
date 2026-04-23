5d7c2f650481f5cd4d2ff8fbb75c9df6640c9d19
pokekevin <pokekevin@outlook.com>
2026-04-22 18:41:17 -0700

    feytemp spinal surgery and misc. party.gd additions

--- party/party.gd
+++ party/party.gd
@@ -81,8 +81,20 @@ func remove_item(item_address: String, amount: int):
 
 
 var company: Dictionary = {
-	"Rank": 1
+	"Rank": 1,
 	#F-E-D-C-B-A-S-U
+	#numbers for easier checks on unlocks
+	
+	"overall_rep" : 0,
+	"guild_rep": 0,
+	"syndicate_rep": 0,
+	"business_rep": 0,
+	
+	"location": "4",
+	#2,3,4,maria,vermillion,heaven
+	
+	"business_type": "none",
+	"business_xp": 0,
 	
 }
 
@@ -150,6 +162,31 @@ var kyuki: Dictionary = {
 	"luck": 0,
 }
 
+var kiana: Dictionary = {
+	"doll_base": ResourceLoader.load("res://dolls/dolls/kiana/kiana.tres"),
+	#resource with static info about doll
+	"unlock": 1,
+	"death": 0,
+	#0 for false, 1 for true
+	
+	#info
+	"level": 1,
+	"xp": 0,
+	"class": ResourceLoader.load("res://dolls/classes/Dummy.tres"),
+	#unpunct, needs to load in during expedition select
+	
+	#stats
+	"dom": 0,
+	"inf": 0,
+	"res": 0,
+	"tem": 0,
+	"pre": 0,
+	
+	#other attributes
+	"affinity": 0,
+	"luck": 0,
+}
+
 var kosellia: Dictionary = {
 	"doll_base" : ResourceLoader.load("res://dolls/dolls/kosellia/kosellia.tres"),
 	"unlock": 1,
@@ -183,10 +220,11 @@ var kosellia: Dictionary = {
 func doll_gain(doll):
 	doll_list.append(doll)
 #change this when you add a new doll
-var doll_list: Array = [kyuki, kosellia, kyuki, kyuki]
+var doll_list: Array = [kyuki, kosellia, kiana, kyuki]
 
 #final savefile that everything else needs to go under
 var savefile: Dictionary
+#save
 func update_save():
 	savefile = {
 	"doll_list" : doll_list,
@@ -204,9 +242,11 @@ func update_save():
 	
 	"kei": kei,
 	"kyuki": kyuki,
+	"kiana": kiana,
 	"kosellia": kosellia,
 }
 
+#load
 func update_party():
 	doll_list = savefile["doll_list"]
 	
@@ -222,4 +262,5 @@ func update_party():
 	
 	kei = savefile["kei"]
 	kyuki = savefile["kyuki"]
+	kiana = savefile["kiana"]
 	kosellia = savefile["kosellia"]
